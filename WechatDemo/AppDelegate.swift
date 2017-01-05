2968c747844a3e7f1188de771d401dce6f55553b
WalkWithWind <956035125@qq.com>
2017-01-05 14:52:22 +0800

    Update AppDelegate.swift

--- WechatDemo/AppDelegate.swift
+++ WechatDemo/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        NSApp.applicationIconImage = NSImage(named: "AppIcon")
+        //NSApp.applicationIconImage = NSImage(named: "AppIcon")
         status.image = NSImage(named: "wechat")
     }
 
